799e9e3b900648572b4afb2396fe9db913c3586a
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-19 16:52:10 +0100

    Creat newsletter and footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,141 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>E-commerce</Logo>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men's Fashion</ListItem>
+          <ListItem>Women's Fashion</ListItem>
+          <ListItem>Accesories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Shipping and Returns</ListItem>
+          <ListItem>Wishist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> 123 5th Avenue, New York, NY
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +1 123 456 789
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> contact@ecommerce.com
+        </ContactItem>
+        <ContactItem>
+          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+        </ContactItem>
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
