e7929325a942864be7ceb397c1deb80f3380fa93
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-19 14:50:59 +0100

    Create slider and announcement components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+    </div>
+  );
 };
 export default Home;
