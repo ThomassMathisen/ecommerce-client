305efaab739e840157122a4682596ca977f6da0e
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-19 16:30:57 +0100

    Create products component

--- src/data.js
+++ src/data.js
@@ -43,11 +43,11 @@ export const categories = [
 export const popularProducts = [
   {
     id: 1,
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    img: "https://www.prada.com/content/dam/pradanux_products/S/SC4/SC449/1WQ8F0002/SC449_1WQ8_F0002_S_182_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.500.500.jpg",
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "https://cdn-images.farfetch-contents.com/17/05/05/66/17050566_34235282_300.jpg",
   },
   {
     id: 3,
@@ -55,7 +55,7 @@ export const popularProducts = [
   },
   {
     id: 4,
-    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    img: "https://img01.ztat.net/article/spp-media-p1/93bac544c6534eca9835241591155ca2/18a906bbed794d10bf9082780bf5fbd7.jpg?imwidth=762&filter=packshot",
   },
   {
     id: 5,
@@ -63,11 +63,11 @@ export const popularProducts = [
   },
   {
     id: 6,
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    img: "https://cdn-images.farfetch-contents.com/17/80/99/14/17809914_38535901_300.jpg",
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://cdn.fashiola.no/L561610711/emporio-armani-herre-capser-embroidered-logo-baseball-cap.jpg",
   },
   {
     id: 8,
