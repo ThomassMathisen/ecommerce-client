abc744753d077c6133b679bbdd1588898091f9be
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-22 11:58:01 +0100

    Add responsive design

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -32,17 +34,19 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div``;
-
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })}
+`;
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
-  margin: 0 10px;
+  margin: 0px 10px;
 `;
 
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -52,6 +56,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -60,10 +65,7 @@ const ProductDetail = styled.div`
 `;
 
 const Image = styled.img`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  width: 200px;
 `;
 
 const Details = styled.div`
@@ -72,6 +74,7 @@ const Details = styled.div`
   flex-direction: column;
   justify-content: space-around;
 `;
+
 const ProductName = styled.span``;
 
 const ProductId = styled.span``;
@@ -79,9 +82,8 @@ const ProductId = styled.span``;
 const ProductColor = styled.div`
   width: 20px;
   height: 20px;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
 `;
 
 const ProductSize = styled.span``;
@@ -98,17 +100,18 @@ const ProductAmountContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
-  cursor: pointer;
 `;
 
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
 `;
 
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
 `;
 
 const Hr = styled.hr`
@@ -147,7 +150,6 @@ const Button = styled.button`
   background-color: black;
   color: white;
   font-weight: 600;
-  cursor: pointer;
 `;
 
 const Cart = () => {
@@ -172,14 +174,14 @@ const Cart = () => {
                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                 <Details>
                   <ProductName>
-                    <b>Product:</b> THUNDER SHOES
+                    <b>Product:</b> JESSIE THUNDER SHOES
                   </ProductName>
                   <ProductId>
-                    <b>ID:</b> 91391323123
+                    <b>ID:</b> 93813718293
                   </ProductId>
                   <ProductColor color="black" />
                   <ProductSize>
-                    <b>Size:</b> 44
+                    <b>Size:</b> 37.5
                   </ProductSize>
                 </Details>
               </ProductDetail>
@@ -227,11 +229,11 @@ const Cart = () => {
             </SummaryItem>
             <SummaryItem>
               <SummaryItemText>Estimated Shipping</SummaryItemText>
-              <SummaryItemPrice>$ 5</SummaryItemPrice>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
               <SummaryItemText>Shipping Discount</SummaryItemText>
-              <SummaryItemPrice>$ -5</SummaryItemPrice>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem type="total">
               <SummaryItemText>Total</SummaryItemText>
